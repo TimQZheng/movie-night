88431726f7cd0b2932c745ebc1f903eb7c317eff
TimQZheng <Tim.Q.Zheng@gmail.com>
2023-05-02 00:27:15 -0400

    Update next.config.js
    change assetprefix to basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/movie-night/',
+  basePath: '/movie-night',
 }
 
 module.exports = nextConfig
